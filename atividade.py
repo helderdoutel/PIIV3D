a106652589fa51d69bf524b5827913c13ae8aa16
Helder Gomes Doutel <heldergomesdoutel@gmail.com>
2018-12-07 23:05:00 -0200

    py para atividade

---
+++ atividade.py
@@ -0,0 +1,12 @@
+import random
+import time
+import datetime
+import numpy as np
+import glfw
+import OpenGL
+OpenGL.ERROR_CHECKING = False
+OpenGL.FULL_LOGGING = True
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+print('teste')
