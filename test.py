4c2958d7bf946a4e8063a9edfae810ad809b9ebe
Alex Shun Yamazaki <alex.yamazaki@outlook.com>
2018-12-07 23:02:08 -0200

    +test

--- test.py
+++ test.py
@@ -1,214 +1,147 @@
-# author: Somsubhra Bairi (201101056)
-
-# Draw sphere with QUAD_STRIP
-# Controls: UP/DOWN - scale up/down
-#           LEFT/RIGHT - rotate left/right
-#           F1 - Toggle surface as SMOOTH or FLAT
-
-# Python imports
-from math import *
-
-# OpenGL imports for python
-try:
-    from OpenGL.GL import *
-    from OpenGL.GLU import *
-    from OpenGL.GLUT import *
-except:
-    print ("OpenGL wrapper for python not found")
-
-# Last time when sphere was re-displayed
-last_time = 0
+# import PyOpenGl
+import time
+import glfw
+import time
+import OpenGL
+OpenGL.ERROR_CHECKING = False
+OpenGL.FULL_LOGGING = True
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 
-# The sphere class
-class Sphere:
+verticesOrigem = (
+    (0.50, -0.50, -0.50),
+    (0.50, 0.50, -0.50),
+    (-0.50, 0.50, -0.50),
+    (-0.50, -0.50, -0.50),
+    (0.50, -0.50, 0.50),
+    (0.50, 0.50, 0.50),
+    (-0.50, -0.50, 0.50),
+    (-0.50, 0.50, 0.50)
+)
 
-    # Constructor for the sphere class
-    def __init__(self, radius):
+pessoaOrigem = (
+    (0, 0.5, 0),
+    (1, 0.5, 0),
+    (0.5, 0.5, 0.5),
+    (0.5,2, 1),
+)
 
-        # Radius of sphere
-        self.radius = radius
+pessoaArestas = (
+    (0,1),
+    (1,2),
+    (2,0),
+    (3,0),
+    (3,1),
+    (3,2)
+)
 
-        # Number of latitudes in sphere
-        self.lats = 100
+edges = (
+    (0, 1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7)
+)
 
-        # Number of longitudes in sphere
-        self.longs = 100
-
-        self.user_theta = 0
-        self.user_height = 0
-
-        # Direction of light
-        self.direction = [0.0, 2.0, -1.0, 1.0]
-
-        # Intensity of light
-        self.intensity = [0.7, 0.7, 0.7, 1.0]
-
-        # Intensity of ambient light
-        self.ambient_intensity = [0.3, 0.3, 0.3, 1.0]
-
-        # The surface type(Flat or Smooth)
-        self.surface = GL_FLAT
-
-    # Initialize
-    def init(self):
-
-        # Set background color to black
-        glClearColor(0.0, 0.0, 0.0, 0.0)
-
-        self.compute_location()
-
-        # Set OpenGL parameters
-        glEnable(GL_DEPTH_TEST)
-
-        # Enable lighting
-        glEnable(GL_LIGHTING)
-
-        # Set light model
-        glLightModelfv(GL_LIGHT_MODEL_AMBIENT, self.ambient_intensity)
-
-        # Enable light number 0
-        glEnable(GL_LIGHT0)
-
-        # Set position and intensity of light
-        glLightfv(GL_LIGHT0, GL_POSITION, self.direction)
-        glLightfv(GL_LIGHT0, GL_DIFFUSE, self.intensity)
-
-        # Setup the material
-        glEnable(GL_COLOR_MATERIAL)
-        glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE)
-
-    # Compute location
-    def compute_location(self):
-        x = 2 * cos(self.user_theta)
-        y = 2 * sin(self.user_theta)
-        z = self.user_height
-        d = sqrt(x * x + y * y + z * z)
-
-        # Set matrix mode
-        glMatrixMode(GL_PROJECTION)
-
-        # Reset matrix
-        glLoadIdentity()
-        glFrustum(-d * 0.5, d * 0.5, -d * 0.5, d * 0.5, d - 1.1, d + 1.1)
-
-        # Set camera
-        gluLookAt(x, y, z, 0, 0, 0, 0, 0, 1)
-
-    # Display the sphere
-    def display(self):
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-        # Set color to white
-        glColor3f(1.0, 1.0, 1.0)
-
-        # Set shade model
-        glShadeModel(self.surface)
-
-        self.draw()
-        glutSwapBuffers()
-
-    # Draw the sphere
-    def draw(self):
-        for i in range(0, self.lats + 1):
-            lat0 = pi * (-0.5 + float(float(i - 1) / float(self.lats)))
-            z0 = sin(lat0)
-            zr0 = cos(lat0)
-
-            lat1 = pi * (-0.5 + float(float(i) / float(self.lats)))
-            z1 = sin(lat1)
-            zr1 = cos(lat1)
-
-            # Use Quad strips to draw the sphere
-            glBegin(GL_QUAD_STRIP)
-
-            for j in range(0, self.longs + 1):
-                lng = 2 * pi * float(float(j - 1) / float(self.longs))
-                x = cos(lng)
-                y = sin(lng)
-                glNormal3f(x * zr0, y * zr0, z0)
-                glVertex3f(x * zr0, y * zr0, z0)
-                glNormal3f(x * zr1, y * zr1, z1)
-                glVertex3f(x * zr1, y * zr1, z1)
-
-            glEnd()
-
-    # Keyboard controller for sphere
-    def special(self, key, x, y):
-
-        # Scale the sphere up or down
-        if key == GLUT_KEY_UP:
-            self.user_height += 0.1
-        if key == GLUT_KEY_DOWN:
-            self.user_height -= 0.1
-
-        # Rotate the cube
-        if key == GLUT_KEY_LEFT:
-            self.user_theta += 0.1
-        if key == GLUT_KEY_RIGHT:
-            self.user_theta -= 0.1
-
-        # Toggle the surface
-        if key == GLUT_KEY_F1:
-            if self.surface == GL_FLAT:
-                self.surface = GL_SMOOTH
-            else:
-                self.surface = GL_FLAT
-
-        self.compute_location()
-        glutPostRedisplay()
-
-    # The idle callback
-    def idle(self):
-        global last_time
-        time = glutGet(GLUT_ELAPSED_TIME)
-
-        if last_time == 0 or time >= last_time + 40:
-            last_time = time
-            glutPostRedisplay()
-
-    # The visibility callback
-    def visible(self, vis):
-        if vis == GLUT_VISIBLE:
-            glutIdleFunc(self.idle)
-        else:
-            glutIdleFunc(None)
+elevadores = []
 
 
-# The main function
+#Gerar os vertices dos elevadores lado lado
+def GerarElevadores(quantidade, espacamento_inicial):
+    espacamento = espacamento_inicial
+    for i in range(quantidade):
+        tupla = tuple([(i[0]+espacamento,i[1],i[2]) for i in verticesOrigem])
+        elevadores.append(tupla)
+        #print(tupla)
+        espacamento += espacamento_inicial
+
+#Move elevador para cima e para baixo
+def MoverElevador(index,direcao,velocidade):
+    if(direcao):
+        elevadores[index] = tuple([(i[0],i[1]+velocidade,i[2]) for i in elevadores[index]])        
+    else:
+        elevadores[index] = tuple([(i[0],i[1]-velocidade,i[2]) for i in elevadores[index]])
+
+
+#Desenha os elevadores(Retangulos) e as pessoas(esferas)
+def Desenhar():
+    glBegin(GL_LINES)
+    for edge in pessoaArestas:
+        for vertex in edge:
+                glVertex3fv(pessoaOrigem[vertex])              
+    for elevador in elevadores:
+        for edge in edges:
+            for vertex in edge:
+                glVertex3fv(elevador[vertex])      
+    glEnd()
+
+
+
 def main():
 
-    # Initialize the OpenGL pipeline
-    glutInit(sys.argv)
+    
+    GerarElevadores(3,5)
 
-    # Set OpenGL display mode
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH)
+    if not glfw.init():
+        return
 
-    # Set the Window size and position
-    glutInitWindowSize(300, 300)
-    glutInitWindowPosition(50, 100)
+    window = glfw.create_window(1920, 1080, 'Simulação 3D', None, None)
 
-    # Create the window with given title
-    glutCreateWindow('Sphere')
+    if not window:
+        glfw.terminate()
+        return
 
-    # Instantiate the sphere object
-    s = Sphere(1.0)
+    glfw.make_context_current(window)
 
-    s.init()
+    gluPerspective(90, (1920 / 1080), 0.,25.0)
 
-    # Set the callback function for display
-    glutDisplayFunc(s.display)
+    glTranslatef(-15, 5, -15)
+    glRotatef(75, 10, 0, 0)
 
-    # Set the callback function for the visibility
-    glutVisibilityFunc(s.visible)
+    subidas = [True,True,True,True,True,True]
+    mover = [True,False,False]
+    
+    velocidade = 0.10
 
-    # Set the callback for special function
-    glutSpecialFunc(s.special)
+        
 
-    # Run the OpenGL main loop
-    glutMainLoop()
+    while not glfw.window_should_close(window):
 
+        for index in range(len(elevadores)):
+            if(subidas[index]):
+                if(mover[index]):
+                    MoverElevador(index,True,velocidade)  
+                if(elevadores[0][0][1] > 5):
+                    mover[1] = True
+                if(elevadores[1][0][1] > 7):
+                    mover[2] = True                   
+                if(elevadores[index][0][1] > 10):
+                    subidas[index] = False
 
-# Call the main function
-if __name__ == '__main__':
-main()
+            else:
+                if(mover[index]):
+                    MoverElevador(index,False,velocidade)    
+                if(elevadores[index][0][1] < 1):
+                    subidas[index] = True
+
+        # glRotatef(75, 10, 0, 0)
+        time.sleep(0.10)
+
+         
+        glfw.poll_events()
+        glfw.swap_buffers(window)
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        Desenhar()
+
+    glfw.terminate()
+
+if __name__ == "__main__": main()
