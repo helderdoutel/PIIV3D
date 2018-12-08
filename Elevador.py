00621396de2a4682623ae11d99315f0d1dcb8ebb
Helder Gomes Doutel <heldergomesdoutel@gmail.com>
2018-12-08 00:14:22 -0200

    Classes de objetos

---
+++ Elevador.py
@@ -0,0 +1,50 @@
+class Elevador(object):
+
+    def __init__(self):
+        """Construtor."""
+        self.ultima_partida = None
+        self.tempo_viagem = None
+        self.passageiros = []
+        self.quebrado_hora = None
+        self.viagens = 0
+        self.vertices = None
+
+    def get_ultima_partida(self):
+        """."""
+        return self.ultima_partida
+
+    def set_ultima_partida(self, ultima_partida):
+        """."""
+        self.ultima_partida = ultima_partida
+
+    def get_tempo_viagem(self):
+        """."""
+        return self.tempo_viagem
+
+    def set_tempo_viagem(self, tempo_viagem):
+        """."""
+        self.tempo_viagem = tempo_viagem
+
+    def get_passageiros(self):
+        """."""
+        return self.passageiros
+
+    def set_passageiros(self, passageiros):
+        """."""
+        self.passageiros = passageiros
+
+    def get_quebrado_hora(self):
+        """."""
+        return self.quebrado_hora
+
+    def set_quebrado_hora(self, quebrado_hora):
+        """."""
+        self.quebrado_hora = quebrado_hora
+
+    def get_viagens(self):
+        """."""
+        return self.viagens
+
+    def set_viagens(self, viagens):
+        """."""
+        self.viagens = viagens
---
+++ Passageiro.py
@@ -0,0 +1,23 @@
+class Passageiro(object):
+
+    def __init__(self, hora_chegada=None, hora_elevador=None):
+        """."""
+        self.hora_chegada = hora_chegada
+        self.hora_elevador = hora_elevador
+        self.vertices = 
+
+    def get_hora_chegada(self):
+        """."""
+        return self.hora_chegada
+
+    def set_hora_chegada(self, hora_chegada):
+        """."""
+        self.hora_chegada = hora_chegada
+
+    def get_hora_elevador(self):
+        """."""
+        return self.hora_elevador
+
+    def set_hora_elevador(self, hora_elevador):
+        """."""
+        self.hora_elevador = hora_elevador
